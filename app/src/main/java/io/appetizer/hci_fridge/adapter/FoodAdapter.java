2c2d2bb79cdd01e7e3a8e4322ba09efe5433eed2
dingzhenkai <13511577582@163.com>
2018-06-06 00:13:15 +0800

    realize food edit and remove

--- app/src/main/java/io/appetizer/hci_fridge/adapter/FoodAdapter.java
+++ app/src/main/java/io/appetizer/hci_fridge/adapter/FoodAdapter.java
@@ -1,7 +1,9 @@
 package io.appetizer.hci_fridge.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,42 +21,126 @@ import io.appetizer.hci_fridge.R;
  * Created by user on 2018/5/31.
  */
 
-public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ItemViewHolder> {
+
     private List<Foodinfo> data;
-    public FoodAdapter(List<Foodinfo> objects) {
+    private LayoutInflater mInflater;
+    private OnItemClickListener mOnItemClickListener;
+
+    public FoodAdapter(Context context, List<Foodinfo> objects) {
         this.data = objects;
+        mInflater = LayoutInflater.from(context);
     }
 
-    //创建新View，被LayoutManager所调用
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.fridge_item,viewGroup,false);
-        ViewHolder vh = new ViewHolder(view);
-        return vh;
-    }
-    //将数据与界面进行绑定的操作
-    @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int position) {
-        Foodinfo food = data.get(position);
-        viewHolder.foodName.setText(food.getName());
-        viewHolder.foodNum.setText(food.getNum()+"");
-        viewHolder.foodImage.setImageResource(food.getImageId());
-    }
-    //获取数据的数量
     @Override
     public int getItemCount() {
         return data.size();
     }
-    //自定义的ViewHolder，持有每个Item的的所有界面元素
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onBindViewHolder(final ItemViewHolder itemViewHolder, final int i) {
+        Foodinfo food = data.get(i);
+        itemViewHolder.foodName.setText(food.getName());
+        itemViewHolder.foodNum.setText(food.getNum()+"");
+        itemViewHolder.foodImage.setImageResource(food.getImageId());
+        if(mOnItemClickListener != null) {
+            /**
+             * 这里加了判断，itemViewHolder.itemView.hasOnClickListeners()
+             * 目的是减少对象的创建，如果已经为view设置了click监听事件,就不用重复设置了
+             * 不然每次调用onBindViewHolder方法，都会创建两个监听事件对象，增加了内存的开销
+             */
+            if(!itemViewHolder.itemView.hasOnClickListeners()) {
+                Log.e("ListAdapter", "setOnClickListener");
+                itemViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int pos = itemViewHolder.getPosition();
+                        mOnItemClickListener.onItemClick(v, pos);
+                    }
+                });
+                itemViewHolder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                    @Override
+                    public boolean onLongClick(View v) {
+                        int pos = itemViewHolder.getPosition();
+                        mOnItemClickListener.onItemLongClick(v, pos);
+                        return true;
+                    }
+                });
+            }
+        }
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        /**
+         * 使用RecyclerView，ViewHolder是可以复用的。这根使用ListView的VIewHolder复用是一样的
+         * ViewHolder创建的个数好像是可见item的个数+3
+         */
+        Log.e("ListAdapter", "onCreateViewHolder");
+        ItemViewHolder holder = new ItemViewHolder(mInflater.inflate(
+                R.layout.fridge_item, viewGroup, false));
+        return holder;
+    }
+
+    /**
+     * 向指定位置添加元素
+     * @param position
+     * @param value
+     */
+    public void add(int position, Foodinfo value) {
+        if(position > data.size()) {
+            position = data.size();
+        }
+        if(position < 0) {
+            position = 0;
+        }
+        data.add(position, value);
+        /**
+         * 使用notifyItemInserted/notifyItemRemoved会有动画效果
+         * 而使用notifyDataSetChanged()则没有
+         */
+        notifyItemInserted(position);
+    }
+
+    /**
+     * 移除指定位置元素
+     * @param position
+     * @return
+     */
+    public String remove(int position) {
+        if(position > data.size()-1) {
+            return null;
+        }
+        Foodinfo value = data.remove(position);
+        notifyItemRemoved(position);
+        return value.toString();
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener mOnItemClickListener) {
+        this.mOnItemClickListener = mOnItemClickListener;
+    }
+
+    /**
+     * 处理item的点击事件和长按事件
+     */
+    public interface OnItemClickListener {
+        public void onItemClick(View view, int position);
+        public void onItemLongClick(View view, int position);
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
         public ImageView foodImage;
         public TextView foodName;
         public TextView foodNum;
-        public ViewHolder(View view){
-            super(view);
-            foodImage = (ImageView) view.findViewById(R.id.icon);
-            foodName = (TextView) view.findViewById(R.id.label);
-            foodNum = (TextView) view.findViewById(R.id.num);
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+            foodImage = (ImageView) itemView.findViewById(R.id.icon);
+            foodName = (TextView) itemView.findViewById(R.id.label);
+            foodNum = (TextView) itemView.findViewById(R.id.num);
         }
     }
 
--- app/src/main/java/io/appetizer/hci_fridge/fragment/fridgeFragment.java
+++ app/src/main/java/io/appetizer/hci_fridge/fragment/fridgeFragment.java
@@ -1,17 +1,26 @@
 package io.appetizer.hci_fridge.fragment;
 
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +36,7 @@ import io.appetizer.hci_fridge.adapter.FoodAdapter;
 public class fridgeFragment extends Fragment {
 
     private List<Foodinfo> foodList = new ArrayList<Foodinfo>();
+    public Context context;
     public fridgeFragment() {
         // Required empty public constructor
     }
@@ -57,21 +67,93 @@ public class fridgeFragment extends Fragment {
     }*/
     private SwipeRefreshLayout mRefreshSrl;
     private RecyclerView mContentRv;
+    private FoodAdapter adapter;
     private View view;
+    private android.support.v7.widget.Toolbar toolbar;
 
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.frag_fridge, container, false);
         initFood();
+        context = this.getContext();
+        //toolbar = (android.support.v7.widget.Toolbar) view.findViewById(R.id.tb_toolbar);
+        //((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
         mContentRv = (RecyclerView) view.findViewById(R.id.rv_content);
         mContentRv.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         mContentRv.setHasFixedSize(true);
-        mContentRv.setAdapter(new FoodAdapter(foodList));
+        adapter = new FoodAdapter(this.getContext(),foodList);
+        adapter.setOnItemClickListener(new FoodAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, final int position) {
+                System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA" + foodList.get(position).getName());
+                View promptsView = inflater.inflate(R.layout.dialog,null);
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+                final EditText userInput = (EditText) promptsView.findViewById(R.id.editTextResult);
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        // get user input and set it to result
+                                        // edit text
+                                        foodList.get(position).setNum(Double.parseDouble(userInput.getText().toString()));
+                                        adapter.notifyDataSetChanged();
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position) {
+                adapter.remove(position); //remove the item
+
+            }
+        });
+        mContentRv.setAdapter(adapter);
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        MenuInflater menuInflater = getActivity().getMenuInflater();
+        menuInflater.inflate(R.menu.frag_fridge, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_action:
+                System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB");
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+
 
 
 
