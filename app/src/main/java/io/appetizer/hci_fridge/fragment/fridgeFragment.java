28a85d419aaf4221036a5bb683313171cd02de78
dingzhenkai <13511577582@163.com>
2018-08-19 08:52:43 +0800

    8.19

--- app/src/main/java/io/appetizer/hci_fridge/fragment/fridgeFragment.java
+++ app/src/main/java/io/appetizer/hci_fridge/fragment/fridgeFragment.java
@@ -1,14 +1,19 @@
 package io.appetizer.hci_fridge.fragment;
 
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -22,10 +27,16 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.bean.ZxingConfig;
+import com.yzq.zxinglibrary.common.Constant;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,6 +46,9 @@ import io.appetizer.hci_fridge.adapter.FoodAdapter;
 import io.appetizer.hci_fridge.util.Urlpath;
 import io.appetizer.hci_fridge.util.okhttpManager;
 import io.appetizer.hci_fridge.util.sharedPreferenceUtil;
+import me.nereo.multi_image_selector.MultiImageSelectorActivity;
+
+import static android.app.Activity.RESULT_OK;
 
 
 /**
@@ -62,6 +76,8 @@ public class fridgeFragment extends Fragment {
     private final static int RETURN_ADD_ITEM_FAILED= 5;
     private final static int RETURN_GET_ITEM_SUCCEED= 6;
     private final static int RETURN_GET_ITEM_FAILED= 7;
+
+    private static final int REQUEST_CODE_SCAN = 1;
 
 
     private Handler requestHandler = new Handler() {
@@ -310,6 +326,57 @@ public class fridgeFragment extends Fragment {
         }
     }
 
+    private void requestPermission(){
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    Manifest.permission.CAMERA,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+        }else{
+            return;
+        }
+    }
+
+    private void scan_code(){
+
+        requestPermission();
+        Intent intent = new Intent(getActivity(), CaptureActivity.class);
+        /*ZxingConfig是配置类
+         *可以设置是否显示底部布局，闪光灯，相册，
+         * 是否播放提示音  震动
+         * 设置扫描框颜色等
+         * 也可以不传这个参数
+         * */
+        ZxingConfig config = new ZxingConfig();
+        config.setPlayBeep(true);//是否播放扫描声音 默认为true
+        config.setShake(true);//是否震动  默认为true
+        config.setDecodeBarCode(false);//是否扫描条形码 默认为true
+        config.setReactColor(R.color.colorPrimary);//设置扫描框四个角的颜色 默认为淡蓝色
+        //config.setFrameLineColor(R.color.colorAccent);//设置扫描框边框颜色 默认无色
+        config.setFullScreenScan(false);//是否全屏扫描  默认为true  设为false则只会在扫描框中扫描
+        intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+        startActivityForResult(intent, REQUEST_CODE_SCAN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // 扫描二维码/条码回传
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+                System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"+data.getStringExtra(Constant.CODED_CONTENT));
+            }
+        }
+    }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -354,6 +421,9 @@ public class fridgeFragment extends Fragment {
                                     }
                                 }).show();
 
+                        break;
+                    case R.id.scan_action:
+                        scan_code();
                         break;
                     default:
                         break;
